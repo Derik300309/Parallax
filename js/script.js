37d6615654b986d73f67bfdae379f8518f83e0cb
Derik300309 <derik3009@gmail.com>
2021-01-24 10:31:41 +0500

    The final version

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//Получаем элемент фона с деревом
+const bgTree = document.getElementById("background-tree");
+ 
+//При движении мышью вызываем функцию, которая меняет положение фона
+document.addEventListener("mousemove", function (e) { MoveBackground(e); });
+ 
+function MoveBackground(e)
+{
+   //Рассчитываем, насколько далеко от начала оси находится курсор: 0 - 0, 0.5 - середина экрана, 1 - ширина экрана (например, 1920)
+   //Затем умножаем получившееся число на 30 - настолько будет сдвигаться фон
+   //Например, если курсор находится посередине страницы (0.5), то при умножении получится 15
+   //Далее отнимаем половину от 30, чтобы фон мог двигаться как влево, так и вправо
+   let offsetX = (e.clientX / window.innerWidth * 40) - 15;
+   let offsetY = (e.clientY / window.innerHeight * 20) - 5;
+ 
+   //Меняем положение фона
+   bgTree.setAttribute("style", "background-position: " + offsetX + "px " + offsetY + "px;");
+}
